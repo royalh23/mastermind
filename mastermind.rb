5122667bdd004b4e8414c1fae70b77c33b368aac
Royal <royalhuseyn902@gmail.com>
2022-02-14 16:19:09 +0900

    Created the mastermind class (the main class of the game)

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,13 @@
 require './colors'
 
-puts "🔴 ⚪ ⚫"
+class Mastermind
+  def run_game
+    while true
+      puts 'Welcome to Mastermind!'.bold
+      break
+    end
+  end
+end
+
+new_game = Mastermind.new
+new_game.run_game
