d2f1297f22ba55d7225aa59d2923d5a52d5be881
Royal <royalhuseyn902@gmail.com>
2022-03-16 23:22:59 +0900

    Used squiggly heredoc for clearer output

--- display.rb
+++ display.rb
@@ -9,43 +9,48 @@ module Display
   end
 
   def introduce_game
-    puts "\nWelcome to Mastermind!".bold
-    puts "\nIn this game, you will be playing against the computer as one " \
-          'of the following:'
-    puts "\n\t- The #{'codebreaker'.gray.bold}"
-    puts "\t- The #{'codemaker'.gray.bold}"
+    puts <<~HEREDOC
+      \n#{'Welcome to Mastermind!'.bold}
+      \nIn this game, you will be playing against the computer as one of the following:
+      \n\t- The #{'codebreaker'.gray.bold}
+      \t- The #{'codemaker'.gray.bold}
+    HEREDOC
   end
 
   def display_rules
-    puts "\nThe #{'codemaker'.gray.bold} makes the code, which is a "
-    puts 'sequence of four colors chosen from the following:'
-    puts "\n#{'  1  '.bg_red} #{'  2  '.bg_green} #{'  3  '.bg_brown} #{'  4  '.bg_blue} #{'  5  '.bg_magenta} #{'  6  '.bg_cyan}"
-    puts "\nAn example of a code:"
-    puts "\n#{'  2  '.bg_green} #{'  4  '.bg_blue} #{'  6  '.bg_cyan} #{'  2  '.bg_green}"
-    puts "\nAs you can see, there can be more than one of the same color."
-    puts "The #{'codebreaker'.gray.bold} attempts to guess the code in 12 " \
-        'or fewer turns.'
-    puts 'After each guess, clues will be given in the form of colors.'
-    puts "\n🔴 means you have one correct color in the correct position."
-    puts "\n⚪ means you have one correct color in the wrong position."
+    puts <<~HEREDOC
+      \nThe #{'codemaker'.gray.bold} makes the code, which is a
+      sequence of four colors chosen from the following:
+      \n#{'  1  '.bg_red} #{'  2  '.bg_green} #{'  3  '.bg_brown} #{'  4  '.bg_blue} #{'  5  '.bg_magenta} #{'  6  '.bg_cyan}
+      \nAn example of a code:
+      \n#{'  2  '.bg_green} #{'  4  '.bg_blue} #{'  6  '.bg_cyan} #{'  2  '.bg_green}
+      \nAs you can see, there can be more than one of the same color.
+      The #{'codebreaker'.gray.bold} attempts to guess the code in 12 or fewer turns.
+      After each guess, clues will be given in the form of colors.
+      \n🔴 means you have one correct color in the correct position.
+      \n⚪ means you have one correct color in the wrong position.
+    HEREDOC
   end
 
-  def display_note 
-    puts "\n#{'Note'.underline.bold} that the clues are in no particular " \
-        "order, so the #{'codebreaker'.gray.bold} won't"
-    puts 'explicitly know which colors have been guessed correctly via the' \
-        ' clues alone.'
+  def display_note
+    puts <<~HEREDOC
+      \n#{'Note'.underline.bold} that the clues are in no particular order, so the #{'codebreaker'.gray.bold} won't
+      explicitly know which colors have been guessed correctly via the clues alone.
+    HEREDOC
   end
 
   def display_last_example
-    puts "\nCode and clues example:"
-    puts "\n#{'  2  '.bg_green} #{'  4  '.bg_blue} #{'  6  '.bg_cyan} #{'  2  '.bg_green}" \
-          '   Clues: 🔴 ⚪ ⚪'
+    puts <<~HEREDOC
+      \nCode and clues example:
+      \n#{'  2  '.bg_green} #{'  4  '.bg_blue} #{'  6  '.bg_cyan} #{'  2  '.bg_green}   Clues: 🔴 ⚪ ⚪
+    HEREDOC
   end
 
   def display_gameplay_choice_question
-    puts "\nWould you like to be the codebreaker or the codemaker?"
-    puts '(Type in 1 to be the codebreaker and 2 to be the codemaker)'
+    puts <<~HEREDOC
+      \nWould you like to be the codebreaker or the codemaker?
+      (Type in 1 to be the codebreaker and 2 to be the codemaker)
+    HEREDOC
   end
 
   def display_starting_of_game
